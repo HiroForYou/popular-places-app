093ecb8f71dad6d10b69d6f9eb304eaf2f4d2168
HiroForYou <csanchezs@uni.pe>
2021-02-15 17:43:40 -0500

    Add Logout Button

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,27 @@ import Container from '@material-ui/core/Container';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
 import Home from './pages/Home/Home';
 
 import { withAuthenticator } from '@aws-amplify/ui-react';
+import { Auth } from 'aws-amplify';
 
 function App() {
+
+    const logout = async () => {
+       await Auth.signOut;     
+       window.location.reload();
+    };
+
     return (
         <div className="popular-places-app">
             <AppBar position='static'>
-                <Toolbar>
+                <Toolbar className="toolbar">
                     <Typography variant='h4' className="toolbar-title">
                         Popular places app
                     </Typography>
+                    <Button color="inherit" onClick={logout}>Logout</Button>
                 </Toolbar>
             </AppBar>
             <Container>
